663dde9ee4353f2a62a24d8055ce5c7884df2b3f
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2026-09-12 08:16:20 +0800

    login functionality

--- backend/login_process.php
+++ backend/login_process.php
@@ -1,23 +1,23 @@
 <?php
 
-session_start();
-require_once "config.php";
+session_start(); //
+require_once "config.php"; // ge point nato si config.php(connection sa database)
 
-$email = $_POST["email"];
-$password = $_POST["password"];
-
-$sql = "SELECT * FROM users WHERE email = '$email'";
+$email = $_POST["email"]; //johndoe@gmail.com
+$password = $_POST["password"]; //user123
+                                        //johndoe@gmail.com
+$sql = "SELECT * FROM users WHERE email = '$email'"; //query
 
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
 
-    $user = mysqli_fetch_assoc($result);
-
+    $user = mysqli_fetch_assoc($result); // kuhaon niya tanang details ne john doe
+                        //stored(reg) = user123
     if (password_verify($password, $user["password"])) {
 
-        $_SESSION["user_id"] = $user["id"];
-        $_SESSION["fullname"] = $user["fullname"];
+        $_SESSION["user_id"] = $user["id"]; // id
+        $_SESSION["fullname"] = $user["fullname"]; //fullname
 
         header("Location: ../index.php");
         exit();
